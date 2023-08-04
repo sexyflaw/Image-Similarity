589bbfaedc2f7007e064d8e40534b5b73f79c439
norman <sexyflaw@gmail.com>
2023-08-04 11:25:43 +0800

    init

--- src/util/py_scene_detect.py
+++ src/util/py_scene_detect.py
@@ -1,7 +1,16 @@
+from typing import List
 from scenedetect import detect, ContentDetector
-scene_list = detect('D:\\download\\Video\\1.mp4', ContentDetector())
-for i, scene in enumerate(scene_list):
-    print('    Scene %2d: Start %s / Frame %d, End %s / Frame %d' % (
-        i+1,
-        scene[0].get_timecode(), scene[0].get_frames(),
-        scene[1].get_timecode(), scene[1].get_frames(),))
+
+def detect_scenes(video_path: str) -> List[List[str]]:
+    scene_list = detect(video_path, ContentDetector())
+    scene_times = []
+
+    for i, scene in enumerate(scene_list):
+        start_time = scene[0].get_timecode()
+        end_time = scene[1].get_timecode()
+        scene_times.append([start_time, end_time])
+
+    return scene_times
+
+scene_times = detect_scenes('D:\\download\\Video\\1.mp4')
+print(scene_times)
