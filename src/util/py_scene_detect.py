d637f11a1b52de5a2bbc3fec07eb410fef576194
norman <sexyflaw@gmail.com>
2023-08-04 11:34:35 +0800

    init

--- src/util/py_scene_detect.py
+++ src/util/py_scene_detect.py
@@ -1,5 +1,7 @@
+import json
 from typing import List
 from scenedetect import detect, ContentDetector
+import os
 
 def detect_scenes(video_path: str) -> List[List[str]]:
     scene_list = detect(video_path, ContentDetector())
@@ -10,7 +12,18 @@ def detect_scenes(video_path: str) -> List[List[str]]:
         end_time = scene[1].get_timecode()
         scene_times.append([start_time, end_time])
 
+    # Get the directory and base filename of the input video file
+    video_dir, video_filename = os.path.split(video_path)
+    video_basename, video_ext = os.path.splitext(video_filename)
+
+    # Create the output filename by appending "_scene.json" to the base filename
+    output_filename = os.path.join(video_dir, video_basename + '_scene.json')
+
+    # Write the scene times to a JSON file
+    with open(output_filename, 'w') as f:
+        json.dump(scene_times, f)
+
     return scene_times
 
-scene_times = detect_scenes('D:\\download\\Video\\1.mp4')
+scene_times = detect_scenes('D:\\download\\Video\\tao.mp4')
 print(scene_times)
